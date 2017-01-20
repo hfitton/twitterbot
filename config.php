d8e4b0396fb6e08e78e2c5d580ee7c57a1116c0d
Helen Fitton <hfitton@live.com>
2017-01-20 16:29:35 +1300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+$config = array(
+	'settings' => array(
+		'oauth_access_token' => "",
+    'oauth_access_token_secret' => "",
+    'consumer_key' => "",
+    'consumer_secret' => ""
+		),
+
+	'twitter_search'  => "tanner", "helen", "northern",
+	'text_strings' => array(
+		"bunnies are awesome",
+		"bunnies r cool.",
+		"I like bunnies too!"
+		)
+	);
+?>
